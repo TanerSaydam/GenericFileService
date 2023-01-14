4fdc4cc923cf33a0914de33dc1be057eef386cb8
taner <taner@DESKTOP-3BJ5GK9>
2023-01-14 03:29:03 +0300

    Add project files.

---
+++ Files/FileSaveToFtpModel.cs
@@ -0,0 +1,15 @@
+﻿namespace GenericFileService.Files
+{
+    public class FileSaveToFtpModel
+    {
+        public FileSaveToFtpModel(string ftpAddress, string userName, string password)
+        {
+            FtpAddress = ftpAddress;
+            UserName = userName;
+            Password = password;
+        }
+        public string FtpAddress { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Files/FileService.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace GenericFileService.Files
+{
+    public static class FileService
+    {
+        public static string FileSaveToServer(IFormFile file, string filePath)
+        {
+            var fileFormat = file.FileName.Substring(file.FileName.LastIndexOf('.'));
+            fileFormat = fileFormat.ToLower();
+            string fileName = Guid.NewGuid().ToString() + fileFormat;
+            string path = filePath + fileName;
+            using (var stream = System.IO.File.Create(path))
+            {
+                file.CopyTo(stream);
+            }
+            return fileName;
+        }
+
+        public static string FileSaveToFtp(IFormFile file, FileSaveToFtpModel fileSaveToFtpModel)
+        {
+            var fileFormat = file.FileName.Substring(file.FileName.LastIndexOf('.'));
+            fileFormat = fileFormat.ToLower();
+            string fileName = Guid.NewGuid().ToString() + fileFormat;
+
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(
+                $"{fileSaveToFtpModel.FtpAddress}{fileName}");
+            request.Credentials = new NetworkCredential(
+                fileSaveToFtpModel.UserName,
+                fileSaveToFtpModel.Password);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+
+            using (Stream ftpStream = request.GetRequestStream())
+            {
+                file.CopyTo(ftpStream);
+            }
+
+            return fileName;
+        }
+
+        public static byte[] FileConvertByteArrayToDatabase(IFormFile file)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                file.CopyTo(memoryStream);
+                var fileBytes = memoryStream.ToArray();
+                string fileString = Convert.ToBase64String(fileBytes);
+                return fileBytes;
+            }
+        }
+
+        public static void FileDeleteToServer(string path)
+        {
+            try
+            {
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public static void FileDeleteToFtp(string path, FileSaveToFtpModel fileSaveToFtpModel)
+        {
+            try
+            {
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(
+                    $"{fileSaveToFtpModel}{path}");
+                request.Credentials = new NetworkCredential(
+                    fileSaveToFtpModel.UserName,
+                    fileSaveToFtpModel.Password);
+                request.Method = WebRequestMethods.Ftp.DeleteFile;
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
